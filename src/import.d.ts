eecbf13e079714525188cfd16bcd1879a6ad0a0a
Leonard Grosoli <hileo77@gmail.com>
2021-06-17 19:11:15 +0200

    initial commit

---
+++ src/import.d.ts
@@ -0,0 +1,5 @@
+
+declare module '*.fbx' {
+  const value: any;
+  export default value;
+}
---
+++ src/index.tsx
@@ -0,0 +1,131 @@
+import './index.sass';
+
+
+
+
+import * as THREE from 'three';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+
+
+import { OrbitControls } from '@three-ts/orbit-controls';
+import model from './untitled.fbx'
+
+let camera: any
+let scene: any
+let renderer: any
+
+const clock = new THREE.Clock();
+
+let mixer: any;
+
+init();
+animate();
+
+function init() {
+
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
+  camera.position.set(100, 200, 300);
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xa0a0a0);
+  scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
+
+  const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+  hemiLight.position.set(0, 200, 0);
+  scene.add(hemiLight);
+
+  const dirLight = new THREE.DirectionalLight(0xffffff);
+  dirLight.position.set(0, 200, 100);
+  dirLight.castShadow = true;
+  dirLight.shadow.camera.top = 180;
+  dirLight.shadow.camera.bottom = - 100;
+  dirLight.shadow.camera.left = - 120;
+  dirLight.shadow.camera.right = 120;
+  scene.add(dirLight);
+
+  // scene.add( new THREE.CameraHelper( dirLight.shadow.camera ) );
+
+  // ground
+  const mesh = new THREE.Mesh(new THREE.PlaneGeometry(2000, 2000), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }));
+  mesh.rotation.x = - Math.PI / 2;
+  mesh.receiveShadow = true;
+  scene.add(mesh);
+
+  const grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
+  // @ts-ignore
+  grid.material.opacity = 0.2;
+  // @ts-ignore 
+  grid.material.transparent = true;
+  scene.add(grid);
+
+  // model
+  const loader = new FBXLoader();
+  loader.load(model, function (object) {
+
+    // @ts-ignore
+    mixer = new THREE.AnimationMixer(object);
+
+    // @ts-ignore
+    const action = mixer.clipAction(object.animations[0]);
+    action.play();
+    //@ts-ignore
+    object.traverse(function (child) {
+      // @ts-ignore
+      if (child.isMesh) {
+
+        child.castShadow = true;
+        child.receiveShadow = true;
+
+      }
+
+    });
+
+    scene.add(object);
+
+  });
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
+  container.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 100, 0);
+  controls.update();
+
+  window.addEventListener('resize', onWindowResize);
+
+  //@ts-ignore
+  // stats = new Stats();
+  // container.appendChild(stats.dom);
+
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+//
+
+function animate() {
+
+  requestAnimationFrame(animate);
+
+  const delta = clock.getDelta();
+
+  if (mixer) mixer.update(delta);
+
+  renderer.render(scene, camera);
+
+  // stats.update();
+
+}
